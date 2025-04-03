ffdbec6a6635d6c82141633cc1eed5f7ea0eac43
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-04-04 00:20:31 +0300

    done lesson 18 homework

--- app/src/main/java/com/example/tms_android/MainActivity.kt
+++ app/src/main/java/com/example/tms_android/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.tms_android
 
-import android.content.Intent
-import android.content.IntentFilter
-import android.net.wifi.WifiManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.util.Log
-import android.view.View
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
