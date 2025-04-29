12ba94bbeb6d264e82400a922e29e9cd0d8f582d
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-04-29 16:57:38 +0300

    Added editor

--- app/src/main/java/com/example/tms_android/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/tms_android/presentation/view/MainActivity.kt
@@ -2,14 +2,11 @@ package com.example.tms_android.presentation.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tms_android.R
 import com.example.tms_android.databinding.ActivityMainBinding
-import com.example.tms_android.presentation.viewmodel.NotesViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
