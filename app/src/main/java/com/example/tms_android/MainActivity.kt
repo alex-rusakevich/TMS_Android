d3bccac2588b220d70190c83e9036f7e48989b18
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2025-05-12 18:12:30 +0300

    Done on-lesson task

--- app/src/main/java/com/example/tms_android/MainActivity.kt
+++ app/src/main/java/com/example/tms_android/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.tms_android
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.example.tms_android.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -21,6 +25,20 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val countdownJob = lifecycleScope.launch {
+            for (i in 10 downTo 0) {
+                delay(1000)
+                binding.countdown.text = i.toString()
+            }
+        }
+
+        binding.stopCountdown.setOnClickListener {
+            if(countdownJob.isActive){
+                countdownJob.cancel()
+                binding.countdown.text = getString(R.string.countdown_stopped)
+            }
         }
     }
 }
